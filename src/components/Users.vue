db849c93ea76887c28e4ac8f59849f08019852df
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-04-03 11:31:52 +0800

    Users page ui ux and define props type

---
+++ src/components/Users.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <table class="users-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>UserName</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Website</th>
+          <th>Company</th>
+        </tr>
+      </thead>
+      <tbody>
+        <td>1</td>
+        <td>Joe</td>
+        <td>user_joe</td>
+        <td>user_joe@gmail.com</td>
+        <td>CA USA</td>
+        <td>+9595959595</td>
+        <td><a href="#" target="_blank">www.website.com</a></td>
+        <td>Testbla</td>
+      </tbody>
+    </table>
+    {{ users }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { User } from "@/types";
+
+const props = defineProps<{
+  users: User[];
+}>();
+
+console.log(props);
+</script>
+
+<style scoped>
+.users-table {
+  border-collapse: collapse;
+}
+
+.users-table th,
+.users-table td {
+  border: 1px solid #fff;
+  padding: 8px;
+}
+</style>
