e0be9fa913ee123f9e5da1ad9ea3b96d19842b2b
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-04-03 11:04:38 +0800

    setup project api base url and users page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, watch, computed } from "vue";
+import Users from "@/components/Users.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Users />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
